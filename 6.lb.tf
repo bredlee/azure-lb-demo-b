1aa0100590f5536e969377190284bc8f2761be5a
bredlee <90170028+bredlee@users.noreply.github.com>
2021-09-29 12:22:04 +0900

    Update 6.lb.tf

--- 6.lb.tf
+++ 6.lb.tf
@@ -1,9 +1,9 @@
-resource "azurerm_lb" "david-lb" {
-  name = "davidlb"
-  location = azurerm_resource_group.david-rg.location
-  resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_lb" "user21-lb" {
+  name = "user21lb"
+  location = azurerm_resource_group.user21-rg.location
+  resource_group_name = azurerm_resource_group.user21-rg.name
   frontend_ip_configuration {
-  name = "davidPublicIPAddress"
-  public_ip_address_id = azurerm_public_ip.david-publicip.id
+  name = "user21PublicIPAddress"
+  public_ip_address_id = azurerm_public_ip.user21-publicip.id
  }
 }
