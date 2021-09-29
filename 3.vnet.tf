956e5a10689fc2a0a84337af468f72640f94390b
bredlee <90170028+bredlee@users.noreply.github.com>
2021-09-29 12:19:42 +0900

    Update 3.vnet.tf

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "david-net" {
-    name = "david-vnet"
+resource "azurerm_virtual_network" "user21-net" {
+    name = "user21-vnet"
     address_space = ["40.0.0.0/16"]
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+    location = azurerm_resource_group.user21-rg.location
+    resource_group_name = azurerm_resource_group.user21-rg.name
 }
