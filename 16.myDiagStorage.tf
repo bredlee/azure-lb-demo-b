d9e73b43c9c8c04e0811bddd4f8526849b6504ab
bredlee <90170028+bredlee@users.noreply.github.com>
2021-09-29 12:41:48 +0900

    Update 16.myDiagStorage.tf

--- 16.myDiagStorage.tf
+++ 16.myDiagStorage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mystorageaccount" {
-    name                = "davidvm1diagestorage1"
-    resource_group_name = azurerm_resource_group.david-rg.name
-    location = azurerm_resource_group.david-rg.location
+    name                = "user21vm1diagestorage1"
+    resource_group_name = azurerm_resource_group.user21-rg.name
+    location = azurerm_resource_group.user21-rg.location
     account_replication_type = "LRS"
     account_tier = "Standard"
 
