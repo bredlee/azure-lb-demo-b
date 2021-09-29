69c5dbd20849eab7628f0333b55ae44f2a661c82
bredlee <90170028+bredlee@users.noreply.github.com>
2021-09-29 12:26:58 +0900

    Update 10.mynsg.tf

--- 10.mynsg.tf
+++ 10.mynsg.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "mynsg" {
     name                = "myNetworkSecurityGroup"
-    location            = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+    location            = azurerm_resource_group.user21-rg.location
+    resource_group_name = azurerm_resource_group.user21-rg.name
     
     security_rule {
         name                       = "SSH"
@@ -11,7 +11,18 @@ resource "azurerm_network_security_group" "mynsg" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = "22"
-        source_address_prefix      = "*"   ## Admin 서버 IP 주소 혹은 접속하는 서버(or PC) IP주소
+        source_address_prefix      = "58.234.0.70/32"   ## Admin 서버 IP 주소 혹은 접속하는 서버(or PC) IP주소
+        destination_address_prefix = "*"
+    }
+        security_rule {
+        name                       = "SSH-Admin"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "20.194.23.73/32"   ## Admin 서버 IP 주소 혹은 접속하는 서버(or PC) IP주소
         destination_address_prefix = "*"
     }
  security_rule {
