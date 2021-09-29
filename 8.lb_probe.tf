73a52ad092ae2095cebb8a50c986e42ac84d613b
bredlee <90170028+bredlee@users.noreply.github.com>
2021-09-29 12:22:55 +0900

    Update 8.lb_probe.tf

--- 8.lb_probe.tf
+++ 8.lb_probe.tf
@@ -1,6 +1,6 @@
-resource "azurerm_lb_probe" "david-lb-probe" {
-    resource_group_name = azurerm_resource_group.david-rg.name
-    loadbalancer_id = azurerm_lb.david-lb.id
+resource "azurerm_lb_probe" "user21-lb-probe" {
+    resource_group_name = azurerm_resource_group.user21-rg.name
+    loadbalancer_id = azurerm_lb.user21-lb.id
     name = "http-probe"
     protocol = "Http"
     request_path = "/"
