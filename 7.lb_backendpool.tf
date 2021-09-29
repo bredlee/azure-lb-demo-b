80e2fc25c3575b4bbf37841c3423c2dc3580165a
bredlee <90170028+bredlee@users.noreply.github.com>
2021-09-29 12:22:32 +0900

    Update 7.lb_backendpool.tf

--- 7.lb_backendpool.tf
+++ 7.lb_backendpool.tf
@@ -1,4 +1,4 @@
-resource "azurerm_lb_backend_address_pool" "david-bpepool" {
-    loadbalancer_id = azurerm_lb.david-lb.id
-    name = "david-BackEndAddressPool"
+resource "azurerm_lb_backend_address_pool" "user21-bpepool" {
+    loadbalancer_id = azurerm_lb.user21-lb.id
+    name = "user21-BackEndAddressPool"
 }
