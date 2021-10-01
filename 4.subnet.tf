388b8c22e037ef5f574186e0ef2f276d09265aa1
bredlee <90170028+bredlee@users.noreply.github.com>
2021-10-01 13:28:15 +0900

    Update 4.subnet.tf

--- 4.subnet.tf
+++ 4.subnet.tf
@@ -2,5 +2,5 @@ resource "azurerm_subnet" "user21-subnet1" {
     name = "user21-mysubnet1"
     resource_group_name = azurerm_resource_group.user21-rg.name
     virtual_network_name = azurerm_virtual_network.user21-net.name
-    address_prefixes = ["40.0.1.0/24"]
+    address_prefixes = ["21.0.1.0/24"]
 }
